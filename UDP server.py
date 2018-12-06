7b0f3b24b6b1179a10af69e9616e419b226ea5f4
xiebin <819058449@qq.com>
2018-12-06 18:14:28 +0800

    4

---
+++ UDP server.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+import logging
+class Server:
+    def __init__(self,ip='127.0.0.1',port=699):
+        self.ip=ip
+        self.port=port
+        self.sock=socket.socket(type=socket.SOCK_DGRAM)
+        self.sock.bind((self.ip,self.port))
+        self.client=set()
+        self.sock.shutdown(how=2)
+        self.event=threading.Event()
+    def start(self):
+        threading.Thread(target=self.__recv,daemon=True).start()
+    def stop(self):
+        self.event.set()
+        self.sock.close()
+    def __recv(self):
+        while not self.event.is_set():
+            data,ipinfo=self.sock.recvfrom(1024)
+            self.client.add(ipinfo)
+            print(data,ipinfo)
+            if data==b'quit':
+                self.client.remove(ipinfo)
+            self.send(data)
+    def send(self,data):
+        for client in self.client:
+            # data = data.encode()
+            self.sock.sendto(data,client)
+if __name__=='__main__':
+    server = Server()
+    server.start()
+    while True:
+        cmd=input('>>>')
+        if cmd =='stop':
+            server.stop()
+            break
+        cmd = cmd.encode()
+        server.send(cmd)
