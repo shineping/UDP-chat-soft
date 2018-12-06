600ad4fc2caaea16844370e1d110ce856e744dfb
xiebin <819058449@qq.com>
2018-12-06 18:24:23 +0800

    j

---
+++ UDP client.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import logging
+class Custom:
+    def __init__(self,ip='127.0.0.1',port=699):
+        self.ip=ip
+        self.port=port
+        self.sock=socket.socket(type=socket.SOCK_DGRAM)
+        self.event=threading.Event()
+    def start(self):
+        self.send('reg')
+        threading.Thread(target=self.__recv).start()
+    def stop(self):
+        self.event.set()
+        self.sock.close()
+    def __recv(self):
+        while not self.event.is_set():
+            data=self.sock.recv(1024)
+            print(data)
+    def send(self,cmd):
+        cmd=cmd.encode()
+        self.sock.sendto(cmd,(self.ip,self.port))
+if __name__=='__main__':
+    custom = Custom()
+    custom.start()
+    while True:
+        cmd=input('>>>')
+        if cmd =='stop':
+            custom.stop()
+            break
+        custom.send(cmd)
